4402be9cdc729ece733d4de1ec47f2c9b5129754
Tracy Cook <tracymcook23@gmail.com>
2019-06-25 10:22:00 -0500

    Part 2: Replace CheeseType with Category.

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -3,6 +3,7 @@ package org.launchcode.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,7 +25,9 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    // Part 2: Replace CheeseType with Category
+    @ManyToOne
+    private Category category;
 
     public Cheese(String name, String description) {
         this.name = name;
@@ -32,6 +35,8 @@ public class Cheese {
     }
 
     public Cheese() { }
+
+    // Getters and Setters
 
     public int getId() {
         return id;
@@ -53,11 +58,11 @@ public class Cheese {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
+    public Category getCategory() {
+        return category;
     }
 
-    public void setType(CheeseType type) {
-        this.type = type;
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
