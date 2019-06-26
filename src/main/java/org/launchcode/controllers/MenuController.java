95a3068699a234dc0b6e74f78e922eaef458a20b
Tracy Cook <tracymcook23@gmail.com>
2019-06-26 00:57:44 -0500

    Part 3: Create Menu Controller Class and Views.

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,32 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "menu")
+public class MenuController {
+
+    @Autowired
+    private MenuDao menuDao;
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+
+    // Request path: /
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "Menus");
+
+        return "menu/index";
+    }
+
+}
