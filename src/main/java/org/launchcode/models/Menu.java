70d62b12b1d405c8b4918648e1fcc0a3923521d0
Tracy Cook <tracymcook23@gmail.com>
2019-06-25 23:52:02 -0500

    Part 3: Create the Menu Class.

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,58 @@
+package org.launchcode.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+public class Menu {
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToMany
+    private List<Cheese> cheeses;
+
+
+    public void addItem(Cheese item) {
+        cheeses.add(item); // adds given item to the list
+    }
+
+    public Menu() { } // empty default constructor
+
+    public Menu(String name) {
+        this.name = name; // accepts a value for and sets name
+    }
+
+
+    // Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+}
